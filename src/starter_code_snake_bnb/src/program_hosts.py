532f1bc7bfbed818bbfc61be465b641433537138
phillyb33 <filipbiernat@gmail.com>
2024-03-19 13:46:25 +0100

    feat: add view bookings as host

--- src/starter_code_snake_bnb/src/program_hosts.py
+++ src/starter_code_snake_bnb/src/program_hosts.py
@@ -1,3 +1,5 @@
+import datetime
+
 from colorama import Fore
 from infrastructure.switchlang import switch
 import infrastructure.state as state
@@ -160,15 +162,31 @@ def update_availability():
 
     success_msg(f'Date added to cage {selected_cage.name}')
 
+
 def view_bookings():
     print(' ****************** Your bookings **************** ')
 
-    # TODO: Require an account
-    # TODO: Get cages, and nested bookings as flat list
-    # TODO: Print details for each
+    if not state.active_account:
+        error_msg("You must log in first to view bookings")
+        return
 
-    print(" -------- NOT IMPLEMENTED -------- ")
+    cages = svc.find_cages_for_user(state.active_account)
 
+    bookings = [
+        (c, b)
+        for c in cages
+        for b in c.bookings
+        if b.booked_date is not None
+    ]
+
+    print("You have {} bookings.".format(len(bookings)))
+    for c, b in bookings:
+        print(" * Cage: {}, booked date: {}, from {} for {} days.".format(
+            c.name,
+            datetime.date(b.booked_date.year, b.booked_date.month, b.booked_date.day),
+            datetime.date(b.check_in_date.year, b.check_in_date.month, b.check_in_date.day),
+            b.duration_in_days
+        ))
 
 def exit_app():
     print()
